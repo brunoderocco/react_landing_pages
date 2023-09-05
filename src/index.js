a87993e7152308f3b673c59043164000b2585576
brunoderocco <brunoderocco@gmail.com>
2023-09-04 22:53:19 -0300

    Storybook configuration

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import Home from './templates/App';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
