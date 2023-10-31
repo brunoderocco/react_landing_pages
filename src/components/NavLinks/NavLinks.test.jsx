76ff2ce524702051e25710dafbcc6d78cf367940
brunoderocco <brunoderocco@gmail.com>
2023-10-31 20:26:50 -0300

    NavLink Test

--- src/components/NavLinks/NavLinks.test.jsx
+++ src/components/NavLinks/NavLinks.test.jsx
@@ -2,9 +2,26 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { NavLinks } from '.';
 
+import mock from './mock';
+import { theme } from '../../styles/theme';
+
 describe('<NavLinks />', () => {
-  it('should render', () => {
-    renderTheme(<NavLinks>Children</NavLinks>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getAllByRole('link')).toHaveLength(mock.length);
   });
+
+  it('should not render links', () => {
+    renderTheme(<NavLinks />);
+    expect(screen.queryAllByText(/links/i)).toHaveLength(0);
+  });
+
+  it('should render link style', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getByText(/Link 10/i).parentElement).toHaveStyleRule(
+      'flex-flow',
+      'row wrap',
+    );
+  });
+
 });
